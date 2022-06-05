a1d6d63fdf84de6372afe95411f6a94fc41ba54a
Jean-Pierre-UMUGWANEZA <umujp04@gmail.com>
2022-06-05 13:05:00 +0300

    create new branch user-interface

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>JEAN-PIERRE</title>
 </head>
 <body>
     
-<h1>welcome to my site</h1>
+<h1 class='header'>welcome to my site</h1>
 <h2>subtitle</h2>
 
 
